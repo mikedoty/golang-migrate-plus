bc626a655a1edde2095566a7e2fd859b84d4cc02
Mike Doty <onemikedoty@gmail.com>
2024-09-23 11:46:47 -0400

    add func to strip comments from a sql string

--- source/parse.go
+++ source/parse.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -37,4 +38,78 @@ func Parse(raw string) (*Migration, error) {
 		}, nil
 	}
 	return nil, ErrParse
+}
+
+func StripSqlComments(raw string) (string, error) {
+	output := ""
+
+	// inSingleQuoteString := false
+	// inDoubleQuoteString := false
+
+	// inSingleLineComment := false
+	// inMultilineComment := false
+
+	i := 0
+	for i < len(raw) {
+		next1 := string(raw[i])
+		next2 := string(raw[i])
+		if i < len(raw)-1 {
+			next2 = string(raw[i : i+2])
+		}
+
+		// fmt.Println(i)
+		if next2 == "--" {
+			// Found a single line comment (or trailing comment), so
+			// skip ahead to end of string
+			i = findClosingExpressionIndex(raw, i+1, "\n", false)
+
+			// Prefer to preserve the newline -the returned pos goes 1 step past
+			// it, so go back 1 as needed.
+			//
+			// This matches the behavior of multiline, which also does not
+			// "swallow" the final trailing newline (instead leaving a blank line).
+			if i > 0 && raw[i-1] == '\n' {
+				i--
+			}
+
+			// Do prefer to strip trailing whitespace before the end-of-line comment
+			for len(output) > 0 && output[len(output)-1] == ' ' {
+				output = output[0 : len(output)-1]
+			}
+			// fmt.Printf("remaining: '%s'\n", raw[i:])
+		} else if next2 == "/*" {
+			// Found multiline comment, skip entire contents
+			i = findClosingExpressionIndex(raw, i+1, "*/", false)
+			// if i < len(raw) && raw[i] == '\n' {
+			// 	i++
+			// }
+		} else if next1 == "'" || next1 == "\"" {
+			end := findClosingExpressionIndex(raw, i+1, next1, true)
+			output += raw[i:end]
+			i = end
+		} else {
+			output += next1
+			i++
+		}
+	}
+
+	return strings.TrimSpace(output), nil
+}
+
+func findClosingExpressionIndex(s string, posStart int, expr string, isEscaped bool) int {
+	cursor := posStart
+
+	sLength := len(s)
+	exprLength := len(expr)
+
+	for cursor < sLength-exprLength {
+		if s[cursor:cursor+exprLength] == expr {
+			if !isEscaped || s[cursor-1] != '\\' {
+				return cursor + exprLength
+			}
+		}
+		cursor++
+	}
+
+	return cursor + exprLength
 }
